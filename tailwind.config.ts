e1cf6fd84e385f2d28c03da5769eea81b0a0f3bf
Nwanne-san <113237530+Nwanne-san@users.noreply.github.com>
2024-09-06 23:45:45 +0100

    config colors and font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,24 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        sans: ["SF Pro Display"],
+      },
+      colors: {
+        primary: "#1E1E1E",
+        secondary: "#079C4E",
+        yellow: "#FFA500",
+        "background-green": "#013115",
+        gray: {
+          1: "#F4F4F4",
+          2: "#D9D9D9",
+          3: "#5B5B5B",
+        },
+        background: {
+          1: "#F6F5F7",
+          2: "#F2F1F6",
+        },
+      },
     },
   },
   plugins: [],
