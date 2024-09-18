cbca23f753563f37096688e0759491ae899d794d
David <ejeredavid2001@gmail.com>
2024-09-18 01:47:00 +0100

    made some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,13 @@ import OurProducts from "@/components/OurProducts";
 import HeroSection from "@/components/HeroSection";
 import LimitedOffer from "@/components/limoffer";
 import WhyChoose from "@/ui/landing/WhyChoose";
+import Navbar from "@/app/components/Navbar"
 
 export default function Home() {
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-between  font-sans">
-        
+        <Navbar/>
         <HeroSection />
         <OurProducts />
         <LimitedOffer/>
--- src/components/limoffer.tsx
+++ src/components/limoffer.tsx
@@ -11,7 +11,7 @@ const Limoffer = () => {
             Up to 20% off on Groceries and Pastries
           </h1>
           <button className="border border-white rounded-full py-[1em] px-[3em] w-[11em] font-[500] text-[16px] mx-auto md:mx-0">
-            Shop Now
+            Shop Now 
           </button>
         </div>
 
