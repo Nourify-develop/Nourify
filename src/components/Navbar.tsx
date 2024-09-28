a9e31968bffc8c146f97757260fd93b4c66c3b64
Yinye013 <yinadesanya@gmail.com>
2024-09-28 15:48:22 +0100

    feat(shop): add Recently Viewed component to shop section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,11 +6,16 @@ import { CiSearch } from "react-icons/ci";
 import { IoCartOutline } from "react-icons/io5";
 import { RxAvatar } from "react-icons/rx";
 import { usePathname } from "next/navigation";
+import { AnimatePresence, motion } from "framer-motion";
 
 function Navbar() {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const [showLoginMenu, setShowLoginMenu] = useState<boolean>(false);
   const toggleMobileMenu = useCallback(() => {
     setShowMobileMenu((current) => !current);
+  }, []);
+  const toggleLoginMenu = useCallback(() => {
+    setShowLoginMenu((current) => !current);
   }, []);
   const pathname = usePathname();
   const MENU_LINKS = [
@@ -39,7 +44,6 @@ function Navbar() {
           </ul>
         </nav>
         <Logo />
-
         <div className="flex md:gap-[1rem] lg:gap-[2rem] items-center">
           <div className="flex items-center md:gap-[1px] lg:gap-[3px] bg-gray-2 rounded-full ml-4 md:ml-0 md:px-1 md:py-1 lg:px-2 lg:py-2 transition-all duration-300 w-[60%] md:w-[100%]">
             <span>
@@ -58,10 +62,32 @@ function Navbar() {
                 2
               </p>
             </div>
-            <div className="bg-secondary rounded-full md:p-1 lg:p-2">
+            <div
+              onClick={toggleLoginMenu}
+              className="bg-secondary rounded-full md:p-1 lg:p-2"
+            >
               <RxAvatar className="text-white" size={"25px"} />
             </div>
           </div>
+
+          {/* TODO: MAKE A DROPDOWN FOR CREATE ACCOUNT AND LOGIN */}
+          {/* FIXME: WORKS */}
+
+          <AnimatePresence>
+            {showLoginMenu && (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.5 }}
+                className="absolute flex flex-col items-center self-end top-[8rem] right-5 z-[1000] space-y-6 bg-white backdrop-blur-md sm:w-auto sm:self-center py-6  px-8 drop-shadow-md"
+              >
+                <Link href="/signup">Create an account</Link>
+                <Link href="/login">Log in</Link>
+              </motion.div>
+            )}
+            S
+          </AnimatePresence>
 
           {/* TODO: HAMBURGER ICON and add the "OPEN"  class dynamically.*/}
           {/* FIXME: WORKS */}
@@ -78,24 +104,30 @@ function Navbar() {
           {/* TODO: MOBILE MENU - make sure the hidden class is applied and then removed dynamically.*/}
           {/* FIXME: WORKS */}
 
-          <div className="md:hidden">
-            <div
-              className={`${
-                showMobileMenu ? "block" : "hidden"
-              } absolute flex flex-col items-center self-end py-8 mt-14 z-[1000] space-y-6 bg-white backdrop-blur-md sm:w-auto sm:self-center left-6 right-6 drop-shadow-md`}
-            >
-              {MENU_LINKS.map(({ link, name }) => (
-                <Link
-                  href={link}
-                  key={link}
-                  className={`${
-                    pathname === link && "text-secondary font-medium"
-                  } text-[1.125rem] font-medium font-sans transition-all ease-in-out duration-300 text-primary`}
+          <div>
+            <AnimatePresence>
+              {showMobileMenu && (
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.5 }}
+                  className="absolute flex flex-col items-center self-end py-8 mt-20 z-[1000] space-y-6 bg-white backdrop-blur-md sm:w-auto sm:self-center left-6 right-6 drop-shadow-md"
                 >
-                  {name}
-                </Link>
-              ))}
-            </div>
+                  {MENU_LINKS.map(({ link, name }) => (
+                    <Link
+                      href={link}
+                      key={link}
+                      className={`${
+                        pathname === link && "text-secondary font-medium"
+                      } text-[1.125rem] font-medium font-sans transition-all ease-in-out duration-300 text-primary`}
+                    >
+                      {name}
+                    </Link>
+                  ))}
+                </motion.div>
+              )}{" "}
+            </AnimatePresence>
           </div>
         </div>
       </header>
