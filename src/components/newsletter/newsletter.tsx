0a1836fc975f3f1aa493371c196080a44dc24c0f
Nwanne-san <113237530+Nwanne-san@users.noreply.github.com>
2024-10-14 19:26:22 +0100

    refactor: set prettier on newsletter

--- src/components/newsletter/newsletter.tsx
+++ src/components/newsletter/newsletter.tsx
@@ -11,20 +11,17 @@ const newsletter = () => {
       </p>
       <div className="flex justify-center mt-6 w-[80%] md:w-[50%] lg:w-[50%] mx-auto">
         <form
-          
           className="flex items-center w-full backdrop-blur-md border-[#079C4E] px-[3px] py-[2px] md:py-[4px] lg:px-[5px] border-[1px]  rounded-full z-20"
           id="waitlist"
         >
           <input
             type="email"
             placeholder="Email address"
-           
-            
             className="w-full text-[#1E1E1E80] p-3 bg-transparent outline-none placeholder-[#1E1E1E80] md:text-[20px] text-[12px] font-space"
           />
           <button
             className="bg-[#079C4E] text-white font-semibold text-[8px] lg:text-[18px] lg:py-6 px-2 p-3 md:py-5 lg:w-[20em] w-[50%] md:w-[40%] rounded-full hover:bg-green-800 transition ease-linear duration-300  "
-             // Disable the button while loading
+            // Disable the button while loading
           >
             Subscribe
           </button>
