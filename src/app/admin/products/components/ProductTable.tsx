65e467ac32d94b799b4070632e107852058ac333
--global <chikkychime@gmail.com>
2024-11-27 14:42:24 +0100

    Feat: Mapped out the list of products for the admin products page

---
+++ src/app/admin/products/components/ProductTable.tsx
@@ -0,0 +1,101 @@
+import { Column } from "@/types";
+import React, { useState } from "react";
+
+interface ProductTableProps {
+  columns: Column[]; // Array of column definitions
+  data: Record<string, any>[]; // Array of row data
+}
+
+// Format the price with commas and ₦ symbol
+
+const ProductTable: React.FC<ProductTableProps> = ({ columns, data }) => {
+  const [showOptions, setShowOptions] = useState<number | null>(null);
+  const handleOpenOptions = (rowId: number) => {
+    setShowOptions((prevId) => (prevId === rowId ? null : rowId));
+    console.log(showOptions);
+  };
+
+  const formattedPrice = (price: number) =>
+    new Intl.NumberFormat("en-NG", {
+      style: "currency",
+      currency: "NGN",
+      minimumFractionDigits: 0,
+    }).format(price);
+
+  return (
+    <div className="">
+      <table className=" w-full text-left">
+        <thead className="text-gray-5">
+          <tr>
+            {columns.map((col) => (
+              <th key={col.key} className=" font-normal text-base py-5 text-nowrap">
+                {col.header}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row, index) => (
+            <tr
+              key={row.id || index}
+              className=" text-gray-4/90  capitalize font-bold border-t-[.5px] border-primary-2/20 p-5 "
+            >
+              <td className="align-middle py-5 ">
+                <div className="flex justify-start">
+                  <img
+                    src={`/images${row.image}`}
+                    alt={row.name}
+                    className="w-[70px] h-[70px] bg-gray-10 p-2  rounded-[8px]"
+                  />
+                </div>
+              </td>
+              <td className="">
+                <div className="flex flex-col justify-center items-start align-middle py-5">
+                  <span>{row.name}</span>
+                  <span className="text-gray-5 text-xs font-normal">
+                    #{row.productId}
+                  </span>
+                </div>
+              </td>
+              <td className="align-middle py-5">{row.category}</td>
+              <td className="align-middle py-5 text-cente">{row.quantity}</td>
+              <td className="align-middle py-5">
+                {formattedPrice(row.price)}.00
+              </td>
+              <td className="align-middle py-5">
+                <span
+                  className={` rounded-full py-1 px-3 text-xs text-nowrap font-semibold ${
+                    row.status === "In stock"
+                      ? "text-[#14CF3D] bg-[#14CF3D]/10"
+                      : "text-[#EB4E4E] bg-[#EB4E4E]/10"
+                  }`}
+                >
+                  {" "}
+                  {row.status}
+                </span>
+              </td>
+              <td className="relative">
+                <div
+                  onClick={() => handleOpenOptions(row.id)}
+                  className="bg-gray-1 border border-gray-light-2 cursor-pointer rounded-full w-8 h-8 flex justify-center items-center"
+                >
+                  <img src="/icons/options.svg" alt="" />
+                </div>
+                {showOptions === row.id && (
+                  <div className="absolute -left-10 z-10 " >
+                    <div className="bg-white rounded-[15px] shadow-md w-[100px] h-auto  flex flex-col  items-start font-medium">
+                      <button className="text-primary-2 w-full duration-300 hover:bg-gray-1 p-3 text-left">Edit</button>
+                      <button className="text-[#EB4E4E] w-full transition duration-300 hover:bg-gray-1 p-3 text-left ">Delete</button>
+                    </div>
+                  </div>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ProductTable;
--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+"use client";
+import React from "react";
+import ProductTable from "./components/ProductTable";
+import { columns, Products } from "./components/_data";
 
-function page() {
+
+const page = () => {
+
+
   return (
-    <div>page</div>
-  )
-}
+    <>
+   
+      <ProductTable data={Products} columns={columns} />
+    </>
+  );
+};
 
-export default page
+export default page;
