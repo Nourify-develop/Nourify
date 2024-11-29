df8267ea62048a87dab665f9b9b7f4d8023b6299
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2024-11-29 20:51:58 +0100

    feat: updated the products data and pages

--- src/app/shop/[category]/[name]/page.tsx
+++ src/app/shop/[category]/[name]/page.tsx
@@ -1,45 +1,56 @@
-"use client"
-import Wrapper from "@/layout/wrapper"
-import { useParams, usePathname, useSearchParams } from "next/navigation"
-import { products } from "@/ui/products/_data"
-import { reviewDrop, route, size } from "./_data"
-import Link from "next/link"
-import { Fragment, useEffect, useState } from "react"
-import Image from "next/image"
-import Typography from "@/components/typography"
-import { ArrowRight, Dot, Minus, Plus } from "lucide-react"
-import { Button } from "@/components/ui/button"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
-import { CaretRightIcon } from "@radix-ui/react-icons"
-import Maylike from "../../components/alsolike"
+"use client";
+import Wrapper from "@/layout/wrapper";
+import { useParams, usePathname, useSearchParams } from "next/navigation";
+import { products } from "@/ui/products/_data";
+import { reviewDrop, route, size } from "./_data";
+import Link from "next/link";
+import { Fragment, useEffect, useState } from "react";
+import Image from "next/image";
+import Typography from "@/components/typography";
+import { ArrowRight, Dot, Minus, Plus } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { CaretRightIcon } from "@radix-ui/react-icons";
+import Maylike from "../../components/alsolike";
 
 export default function ProductDisplay() {
-  const { category, name } = useParams()
-  const searchParams = useSearchParams()
-  const pathname = usePathname()
-  const [count, setCount] = useState<number>(1)
-  const [selectedSize, setSelectedSize] = useState<string | null>(searchParams.get("size"))
-  const decodedName = decodeURIComponent(`${name}`)
+  const { category, name } = useParams();
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const [count, setCount] = useState<number>(1);
+  const [selectedSize, setSelectedSize] = useState<string | null>(
+    searchParams.get("size")
+  );
+  const decodedName = decodeURIComponent(`${name}`);
   const product = products.find(
     (p) => p.category === category && p.name === decodedName
-  )
+  );
 
   if (!product) {
-    return <div>Product Not Found</div>
+    return <div>Product Not Found</div>;
+  }
+  if (!product.totalReviews) {
+    return <div>No reviews available</div>;
   }
 
-  const routeData = route(category as string, decodedName)
+  const routeData = route(category as string, decodedName);
 
   useEffect(() => {
-    setSelectedSize(searchParams.get("size"))
-  }, [searchParams])
+    setSelectedSize(searchParams.get("size"));
+  }, [searchParams]);
 
   const handleSizeClick = (key: string) => {
-    setSelectedSize(key)
-    const params = new URLSearchParams(searchParams.toString())
-    params.set("size", key)
-    window.history.pushState(null, '', `${pathname}?${params.toString()}`)
-  }
+    setSelectedSize(key);
+    const params = new URLSearchParams(searchParams.toString());
+    params.set("size", key);
+    window.history.pushState(null, "", `${pathname}?${params.toString()}`);
+  };
 
   const totalRatings = product.totalReviews;
   const ratingCounts = {
@@ -56,7 +67,12 @@ export default function ProductDisplay() {
         <p className="text-gray-5">Shop /</p>
         {routeData.map((item, idx) => (
           <Fragment key={idx}>
-            <Link href={item.link} className={`capitalize hover:text-gray-3 ${idx === routeData.length - 1 ? "text-gray-4" : "text-gray-5"}`}>
+            <Link
+              href={item.link}
+              className={`capitalize hover:text-gray-3 ${
+                idx === routeData.length - 1 ? "text-gray-4" : "text-gray-5"
+              }`}
+            >
               {item.title}
             </Link>
             {idx < routeData.length - 1 && <span> / </span>}
@@ -65,35 +81,69 @@ export default function ProductDisplay() {
       </nav>
       <section className="w-full py-6">
         <article className="flex items-center justify-between gap-x-9">
-          <Image src={product.image} width={0} height={0} alt={product.name} className="w-[50%] h-full" />
+          <Image
+            src={product.image}
+            width={0}
+            height={0}
+            alt={product.name}
+            className="w-[50%] h-full"
+          />
           <aside className="w-[50%] flex flex-col gap-y-4">
             <section className="flex flex-col gap-y-6">
-              <Typography.h3 className="!text-gray-12">{product.name}</Typography.h3>
-              <Typography.p className="!text-primary-2/70">{product.description}</Typography.p>
+              <Typography.h3 className="!text-gray-12">
+                {product.name}
+              </Typography.h3>
+              <Typography.p className="!text-primary-2/70">
+                {product.description}
+              </Typography.p>
 
               <span>
-                <small className={`w-max flex items-center gap-x-1 px-2 py-0.5 rounded-2xl ${product.stock === "In Stock" ? "text-green-1 bg-green-1/30" : "text-red-600 bg-red-600/30"}`}><Dot />{product.stock}</small>
+                <small
+                  className={`w-max flex items-center gap-x-1 px-2 py-0.5 rounded-2xl ${
+                    product.status === "In Stock"
+                      ? "text-green-1 bg-green-1/30"
+                      : "text-red-600 bg-red-600/30"
+                  }`}
+                >
+                  <Dot />
+                  {product.status}
+                </small>
                 <span></span>
               </span>
 
-              <Typography.h1 className="!text-gray-12 text-[40px] !font-bold">₦{product.price.toLocaleString()}.00<sub className="text-sm font-normal">/per pack</sub></Typography.h1>
+              <Typography.h1 className="!text-gray-12 text-[40px] !font-bold">
+                ₦{product.price.toLocaleString()}.00
+                <sub className="text-sm font-normal">/per pack</sub>
+              </Typography.h1>
 
               <section className="flex flex-col gap-y-2">
                 <Typography.p isGray>Quantity</Typography.p>
                 <span className="w-max flex items-center gap-x-4 bg-gray-2 px-1.5 py-1 rounded-2xl">
-                  <button type="button" onClick={() => setCount(prev => Math.max(1, prev - 1))}><Minus width={18} /></button>
+                  <button
+                    type="button"
+                    onClick={() => setCount((prev) => Math.max(1, prev - 1))}
+                  >
+                    <Minus width={18} />
+                  </button>
                   {count}
-                  <button type="button" onClick={() => setCount(count + 1)}><Plus width={18} /></button>
+                  <button type="button" onClick={() => setCount(count + 1)}>
+                    <Plus width={18} />
+                  </button>
                 </span>
               </section>
               <section className="flex flex-col gap-y-2">
                 <Typography.p isGray>Size</Typography.p>
                 <span className="flex items-center gap-x-6">
                   {size.map((item) => (
-                    <button type="button"
+                    <button
+                      type="button"
                       key={item.key}
                       onClick={() => handleSizeClick(item.key)}
-                      className={`px-5 py-1.5 rounded-3xl ${selectedSize === item.key ? "bg-gray-6 text-white duration-200" : "bg-gray-2 text-gray-4"}`}
+                      className={`px-5 py-1.5 rounded-3xl ${
+                        selectedSize === item.key
+                          ? "bg-gray-6 text-white duration-200"
+                          : "bg-gray-2 text-gray-4"
+                      }`}
                     >
                       {item.title}
                     </button>
@@ -101,8 +151,15 @@ export default function ProductDisplay() {
                 </span>
               </section>
               <section className="flex flex-col gap-y-4">
-                <Button className="w-full rounded-3xl text-lg xl:text-xl !font-normal">Buy Now</Button>
-                <Button variant="outline" className="w-full rounded-3xl text-lg text-secondary !font-normal">Add to Cart</Button>
+                <Button className="w-full rounded-3xl text-lg xl:text-xl !font-normal">
+                  Buy Now
+                </Button>
+                <Button
+                  variant="outline"
+                  className="w-full rounded-3xl text-lg text-secondary !font-normal"
+                >
+                  Add to Cart
+                </Button>
               </section>
             </section>
           </aside>
@@ -113,7 +170,9 @@ export default function ProductDisplay() {
           <nav className="flex itemc justify-between">
             <aside>
               <Typography.h3 className="!text-gray-4">Reviews</Typography.h3>
-              <Typography.s className="!text-gray-5">({product.reviews.length} out of {product.totalReviews})</Typography.s>
+              <Typography.s className="!text-gray-5">
+                ({product.reviews?.length} out of {product.totalReviews})
+              </Typography.s>
             </aside>
 
             <Select>
@@ -131,16 +190,25 @@ export default function ProductDisplay() {
           </nav>
 
           <span className="flex flex-col gap-y-6">
-            {product.reviews.map((item) => (
+            {product.reviews?.map((item) => (
               <div className="flex flex-col gap-y-2">
                 <span className="flex items-center gap-x-4">
-                  <Image src={"/icons/avatar.svg"} width={0} height={0} className="w-auto h-auto" alt={item.user} />
+                  <Image
+                    src={"/icons/avatar.svg"}
+                    width={0}
+                    height={0}
+                    className="w-auto h-auto"
+                    alt={item?.user || "User avatar"}
+                  />
+
                   <span>
                     <Typography.h4>{item.user}</Typography.h4>
                     <span>{item.rating}</span>
                   </span>
                 </span>
-                <Typography.p isGray className="text-primary-2/70">{item.comment}</Typography.p>
+                <Typography.p isGray className="text-primary-2/70">
+                  {item.comment}
+                </Typography.p>
                 <div className="flex items-center gap-x-4">
                   <span className="text-green-1">Reply</span> <Dot />
                   <span className="text-green-1">View replies</span>
@@ -149,7 +217,9 @@ export default function ProductDisplay() {
             ))}
           </span>
 
-          <button className="w-max px-4 py-2 rounded-full bg-gray-200 text-green-1 text-base ml-auto flex items-center">See all reviews <CaretRightIcon width={24} /> </button>
+          <button className="w-max px-4 py-2 rounded-full bg-gray-200 text-green-1 text-base ml-auto flex items-center">
+            See all reviews <CaretRightIcon width={24} />{" "}
+          </button>
         </aside>
 
         <aside className="w-full">
@@ -157,7 +227,10 @@ export default function ProductDisplay() {
             <Typography.h3 className="!text-gray-4">Ratings</Typography.h3>
             <span>
               <Typography.h3 className="!text-gray-4 !text-2xl">
-                {product.rating} <sub className="text-gray-5 text-base font-normal">({totalRatings} total)</sub>
+                {product.rating}{" "}
+                <sub className="text-gray-5 text-base font-normal">
+                  ({totalRatings} total)
+                </sub>
               </Typography.h3>
             </span>
           </nav>
@@ -168,15 +241,18 @@ export default function ProductDisplay() {
             return (
               <div className="flex items-center gap-x-4 py-2" key={rating}>
                 <span className="text-gray-8 font-bold ">{rating}.0</span>
-                <progress className="w-full h-1.5 !rounded-full " value={percentage} max="100"></progress>
+                <progress
+                  className="w-full h-1.5 !rounded-full "
+                  value={percentage}
+                  max="100"
+                ></progress>
                 <span>{count}</span>
               </div>
             );
           })}
         </aside>
       </section>
-      <Maylike/>
- 
+      <Maylike />
     </Wrapper>
-  )
+  );
 }
