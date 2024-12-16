4573c6490ab9240e3cb62b3bbe3e9bf86ab240c6
Yinye013 <yinadesanya@gmail.com>
2024-12-16 17:46:20 +0100

    fix: shop page responsiveness/functionality configurations

--- src/components/limoffer.tsx
+++ src/components/limoffer.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const Limoffer = () => {
   return (
     <Wrapper className=" bg-white">
-      <div className="text-center gap-2 md:text-left flex flex-col md:flex-row bg-gradient-to-r from-gray-900 via-green-700 to-green-600 rounded-3xl justify-between items-center overflow-hidden md:overflow-visible ">
+      <div className="text-center gap-2 md:text-left flex md:flex-row bg-gradient-to-r from-gray-900 via-green-700 to-green-600 rounded-3xl justify-between items-center overflow-hidden md:overflow-visible ">
         <div className="pl-[2em] py-4 text-white flex flex-col gap-[0.7em] md:w-[50%]">
           <h3 className="text-lg">Limited offer</h3>
           <h1 className="font-[700] text-2xl md:text-[20px] lg:text-4xl">
