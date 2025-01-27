4dd1689bf1c0fccb960712defd7bcdb1a6b6cb24
Nwanne Nnamani <nnamaninwanne@gmail.com>
2025-01-27 19:34:51 +0100

    feat:Implement Orders dashboard with responsive table and editable status, implement sidebar hamburger for small screens

--- src/app/admin/components/sidebar/index.tsx
+++ src/app/admin/components/sidebar/index.tsx
@@ -1,11 +1,13 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { useAuth } from "@/context/AuthContext"; // Import the useAuth hook
 import { Logo } from "../../../../../public/icons";
 import Image from "next/image";
 import sideItems from "@/data/sidebar";
+import { X, Menu } from "lucide-react";
+import { AnimatePresence, motion } from "framer-motion";
 
 interface Iproperties {
   sideNavitems?: {
@@ -24,6 +26,8 @@ const SettingsSidebar: FC<Iproperties> = ({ className }) => {
   const organizationPath = pathname?.split("/")[2];
   const isDashboard = currentPath === "admin" && organizationPath === undefined;
 
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false); // Track mobile menu state
+
   // If user data is not yet loaded, show the skeleton loader
   // if (!user) {
   //   return (
@@ -35,34 +39,96 @@ const SettingsSidebar: FC<Iproperties> = ({ className }) => {
   // }
 
   return (
-    <div
-      className={` ${className} pl-5 pt-5 h-full hidden lg:flex flex-col gap-8 items-start justify-center bg-white-200 w-full md:w-[270px] md:justify-start `}
-    >
-      <Image src={Logo} alt="" />
-      <section className=" flex flex-col gap-y-3 w-full">
-        {sideItems.map((item, index) => (
-          <Link
-            key={index}
-            href={item.link}
-            data-testid={item.id}
-            role="sidebar-link"
-            className={`${
-              currentPath === item.id || (isDashboard && item.id === "admin")
-                ? "bg-active text-primary-2/80 bg-white rounded-lg"
-                : "bg-transparent text-primary-2/50 hover:bg-gray-300"
-            } flex uppercase cursor-pointer items-start justify-start gap-2.5 rounded-lg px-6 py-3 text-lg transition-all duration-300 ease-in md:h-auto md:w-auto md:justify-start md:rounded-sm`}
-          >
-            <Image
-              src={item.icon}
-              alt=""
-              className="h-5 w-5"
-              role="sidebar-icon"
+    <>
+      <button
+        className="lg:hidden fixed top-6 left-6 z-50 p-2 bg-white rounded-md shadow-md"
+        onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+      >
+        {isMobileMenuOpen ? (
+          <X className="h-6 w-6" />
+        ) : (
+          <Menu className="h-6 w-6" />
+        )}
+      </button>
+      <div
+        className={` ${className} pl-5 pt-5 h-full relative hidden lg:flex flex-col gap-8 items-start justify-center bg-white-200 w-full md:w-[200px] xl:w-[270px]  md:justify-start `}
+      >
+        <Image src={Logo} alt="" />
+        <section className=" flex flex-col gap-y-3 w-full">
+          {sideItems.map((item, index) => (
+            <Link
+              key={index}
+              href={item.link}
+              data-testid={item.id}
+              role="sidebar-link"
+              className={`${
+                currentPath === item.id || (isDashboard && item.id === "admin")
+                  ? "bg-active text-primary-2/80 bg-white rounded-lg"
+                  : "bg-transparent text-primary-2/50 hover:bg-gray-300"
+              } flex uppercase cursor-pointer items-start justify-start gap-2.5 rounded-lg px-6 py-3 text-lg transition-all duration-300 ease-in md:h-auto md:w-auto md:justify-start md:rounded-sm`}
+            >
+              <Image
+                src={item.icon}
+                alt=""
+                className="h-5 w-5"
+                role="sidebar-icon"
+              />
+              <span className="text-nowrap text-base">{item.route}</span>
+            </Link>
+          ))}
+        </section>
+      </div>
+      <AnimatePresence>
+        {isMobileMenuOpen && (
+          <>
+            <motion.div
+              initial={{ x: "-100%" }}
+              animate={{ x: 0 }}
+              exit={{ x: "-100%" }}
+              transition={{ type: "spring", stiffness: 300, damping: 30 }}
+              className="fixed top-0 left-0 z-40 w-3/5 sm:w-2/5 h-full bg-white shadow-lg flex flex-col gap-4 p-4 "
+            >
+        <Image src={Logo} alt="" className="ml-10"/>
+
+              <section className=" flex flex-col gap-y-3 w-full">
+                {sideItems.map((item, index) => (
+                  <Link
+                    key={index}
+                    href={item.link}
+                    data-testid={item.id}
+                    role="sidebar-link"
+                    className={`${
+                      currentPath === item.id ||
+                      (isDashboard && item.id === "admin")
+                        ? "bg-active text-primary-2/80 bg-white rounded-lg"
+                        : "bg-transparent text-primary-2/50 hover:bg-gray-300"
+                    } flex uppercase cursor-pointer items-start justify-start gap-2.5 rounded-lg px-6 py-3 text-lg transition-all duration-300 ease-in md:h-auto md:w-auto md:justify-start md:rounded-sm`}
+                  >
+                    <Image
+                      src={item.icon}
+                      alt=""
+                      className="h-5 w-5"
+                      role="sidebar-icon"
+                    />
+                    <span className="text-nowrap text-base">{item.route}</span>
+                  </Link>
+                ))}
+              </section>
+            </motion.div>
+
+            {/* Background Overlay */}
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.3 }}
+              className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm z-30"
+              onClick={() => setIsMobileMenuOpen(false)} // Close sidebar when overlay is clicked
             />
-            <span className="text-nowrap text-base">{item.route}</span>
-          </Link>
-        ))}
-      </section>
-    </div>
+          </>
+        )}
+      </AnimatePresence>
+    </>
   );
 };
 
--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -12,19 +12,19 @@ export default function AdminLayout({
 }) {
   return (
     <ModalProvider>
-      <div className="flex flex-col h-screen bg-gray-100">
+      <div className="flex flex-col h-full bg-gray-100">
         {/* Sidebar */}
-        <div className="flex flex-1">
-          <div className="sticky top-20 z-20">
-            <SettingsSidebar />
+        <div className="flex flex-1 h-full">
+          <div className="sticky top-20 z-20 ">
+            <SettingsSidebar  className="h-full"/>
           </div>
 
           {/* Main content area */}
-          <div className="flex-1 p-4">
+          <div className="flex-1 p-4 bg-gray-100">
             {/* Add border and styling to this wrapper */}
             <div className="w-full h-full border-[1.5px] border-gray-light bg-white rounded-xl">
               {/* Navbar with border */}
-              <div className="border-b border-gray-300">
+              <div className="border-b border-gray-300  md:relative">
                 <UserNavbar />
               </div>
 
